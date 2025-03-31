9b793ab92572e65337763b801be21fef3bd19ca1
Karthikeyan <karthiganesan.gk@gmail.com>
2025-03-31 15:46:12 +0530

    -Changes(part-1)

--- app/src/main/java/com/karthik/android/techtestkmm/BaseApplication.kt
+++ app/src/main/java/com/karthik/android/techtestkmm/BaseApplication.kt
@@ -2,6 +2,11 @@ package com.karthik.android.techtestkmm
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-
+/**
+ * KMM POC
+ *
+ * Base application with hilt setup
+ *
+ */
 @HiltAndroidApp
 class BaseApplication: Application()
